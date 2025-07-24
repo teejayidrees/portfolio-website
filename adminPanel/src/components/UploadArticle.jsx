f03821c365cb61e86425017bacb4d9a87a898c6e
Ahmad Haruna Idris <ahmadgbodoti@gmail.com>
2025-07-24 23:37:43 +0100

    Update UploadArticle.jsx

--- adminPanel/src/components/UploadArticle.jsx
+++ adminPanel/src/components/UploadArticle.jsx
@@ -13,7 +13,6 @@ const UploadArticle = () => {
     preamble: "", // Added for excerpt.preamble
     passage: "", // Added for excerpt.passage
   });
-const token = localStorage.getItem("token");
   const [imageFile, setImageFile] = useState(null); // Stores selected image file
   const [uploading, setUploading] = useState(false); // Upload status
   const [error, setError] = useState(""); // Error message if any
@@ -77,7 +76,6 @@ const token = localStorage.getItem("token");
         {
           headers: {
             "Content-Type": "multipart/form-data", // Important for file uploads
-         Authorization: `Bearer ${token}`,
           },
         }
       );
