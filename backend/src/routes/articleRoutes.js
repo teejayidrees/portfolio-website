4f69c96a01b9463327b5a7f2a7992a64fc2ed346
Ahmad Haruna Idris <ahmadgbodoti@gmail.com>
2025-07-24 23:45:47 +0100

    Update articleRoutes.js

--- backend/src/routes/articleRoutes.js
+++ backend/src/routes/articleRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import multer from "multer";
-import path from "path";
 import {
   createArticle,
   deleteArticle,
@@ -8,30 +7,21 @@ import {
 } from "../Controllers/articleController.js";
 import { verifyAdmin } from "../middleware/authMiddleware.js";
 
-// Create the router
 const router = express.Router();
 
-// Use temporary local disk storage for both audio and image
-// This is needed because we will upload files to Cloudinary manually in the controller
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "temp/"); // All files saved temporarily here
-  },
-  filename: (req, file, cb) => {
-    // Generate unique filename with timestamp and original extension
-    const ext = path.extname(file.originalname);
-    cb(null, `${Date.now()}-${file.fieldname}${ext}`);
-  },
-});
-
-// Initialize multer with the disk storage
+// Use memory storage to avoid saving to disk
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
 router.get("/", getAllArticle);
+
 router.post(
   "/uploads",
   upload.fields([{ name: "image", maxCount: 1 }]),
   verifyAdmin,
   createArticle
 );
+
 router.delete("/:id", verifyAdmin, deleteArticle);
+
 export default router;
